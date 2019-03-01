ca738c7502415f8472abcbbcf7141cf5fba71bb6
ovidiumihaibelciug <ovidiumihaibelciug@gmail.com>
2019-03-01 23:36:18 +0200

    added logos

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,13 +7,16 @@ class Home extends Component {
             <div className={styles['home']}>
                 <div></div>
                 <div className={styles['home__main-content']}>
-                    <iframe
-                        id="player_1"
-                        frameBorder="0"
-                        allowFullScreen="1"
-                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                        title="YouTube video player" width="640" height="360"
-                        src="https://www.youtube.com/embed/?wmode=Opaque&amp;enablejsapi=1&amp;origin=https%3A%2F%2Flistenonrepeat.com&amp;widgetid=1" />
+                    <div className="player">
+                        <iframe
+                            id="player_1"
+                            frameBorder="0"
+                            allowFullScreen="1"
+                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                            title="YouTube video player" width="640" height="360"
+                            src="https://www.youtube.com/embed/?wmode=Opaque&amp;enablejsapi=1&amp;origin=https%3A%2F%2Flistenonrepeat.com&amp;widgetid=1"
+                        />
+                    </div>
                 </div>
                 <div></div>
             </div>
