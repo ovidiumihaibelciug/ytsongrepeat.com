e8ab7cbbc959c9ac74c1fed46dc4359128b3762e
ovidiumihaibelciug <ovidiumihaibelciug@gmail.com>
2019-03-02 02:37:10 +0200

    improvements on the index page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import styles from './Home.module.scss';
+import { Range } from 'rc-slider';
+import { FiEye, FiRepeat } from 'react-icons/fi';
 
 class Home extends Component {
     render() {
@@ -7,15 +9,39 @@ class Home extends Component {
             <div className={styles['home']}>
                 <div></div>
                 <div className={styles['home__main-content']}>
-                    <div className="player">
+                    <div className={styles['player']}>
+                        <div className={styles['player__design']} />
                         <iframe
-                            id="player_1"
+                            loop
+                            width="100%"
+                            height="100%"
+                            src="https://www.youtube.com/embed/y-iwztKUc-E?autoplay=1&loop=1&rel=0&iv_load_policy=3&playlist=y-iwztKUc-E"
                             frameBorder="0"
-                            allowFullScreen="1"
                             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                            title="YouTube video player" width="640" height="360"
-                            src="https://www.youtube.com/embed/?wmode=Opaque&amp;enablejsapi=1&amp;origin=https%3A%2F%2Flistenonrepeat.com&amp;widgetid=1"
-                        />
+                            allowFullScreen>
+                        </iframe>
+                    </div>
+                    <div className={styles['player__info']}>
+                        <div className={styles['player__info__description']}>
+                            <div className={styles['player__info__description__item']}>
+                                <FiEye className={styles['player__info__description__item__icon']} />
+                                <div className={styles['player__info__description__item__text']}>
+                                    7,234,002 views
+                                </div>
+                            </div>
+                            <div className={styles['player__info__description__item']}>
+                                <FiRepeat className={styles['player__info__description__item__icon']} />
+                                <div className={styles['player__info__description__item__text']}>
+                                    3,456 repeats
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles['player__info__slider']}>
+                            <Range defaultValue={[0, 100]} />
+                            <div className={styles['player__info__slider__description']}>
+                                Loop any section of the video using the slider!
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div></div>
